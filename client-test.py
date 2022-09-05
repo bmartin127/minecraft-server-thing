2a1a3b5b1abdcb6d38ff8c45dba4d5df10097b14
Jeff Martin <jmartin127@gmail.com>
2022-09-05 14:36:19 -0600

    bradley code initial

---
+++ client-test.py
@@ -0,0 +1,61 @@
+#core
+data = 0
+conn = 0
+from dataclasses import dataclass
+import socket
+import uuid
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    def connect_base():
+        HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+        PORT = 5021  # Port to listen on (non-privileged ports are > 1023)
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            USERNAME = str(uuid.uuid4())
+            PASSWORD = str(uuid.uuid4())
+            print("username:", USERNAME,)
+            print("password:", PASSWORD)
+            print("connecting to:", PORT)
+            print("waiting for connectoin use the command","{connectcore",USERNAME,PASSWORD,HOST,PORT,"}to connect")
+            s.bind((HOST, PORT))
+            s.listen()
+            conn, addr = s.accept()
+            with conn:
+                print(f"Connected by {addr}")
+                data = conn.recv(1024)
+                if (data.decode()).strip() == USERNAME.strip():
+                    conn.sendall(b"correct")
+                    print("username correct")
+                    data = conn.recv(1024)
+                    print("compare: **",(data.decode()).strip(),"**",PASSWORD.strip(),"**")
+                    if (data.decode()).strip() == PASSWORD.strip():
+                        conn.sendall(b"correct")
+                        print("password correct")
+                        print("connect succsesfully")
+                    else:
+                        conn.sendall(b"incorrent")
+                        print("password incorrect")
+                        print(data.decode(),"was used for password")
+                else: 
+                    conn.sendall(b"incorent")
+                    print("username incorrect")
+                    print(data.decode(),"was used for username")
+
+    def getcommandtyped():
+        commandinput = input().split()
+        commandtype = (commandinput[0])
+        if commandtype == ("connectbase"):
+            connect_base()
+            
+    while 1 == 1:
+        getcommandtyped()
+
+    while 1 == 1:
+        data = conn.recv(1024)
+        if data.decode() == "disconnect":
+            s.close()
+
+            
+
+            
+
+
---
+++ server-test.py
@@ -0,0 +1,64 @@
+#base
+from re import S
+import uuid
+import socket
+
+connected = False
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    def connect_core(USERID, PASID, ADDRESS, PORT):
+            print("connecting")
+            PORT = int(PORT)
+            s.connect((ADDRESS, PORT))
+            s.sendall(str.encode(USERID))
+            data = s.recv(1024)
+            print(data)
+            if data.decode() == "correct":
+                s.sendall(str.encode(PASID))
+                data = s.recv(1024)
+                print(data)
+                if data.decode() == "correct":
+                    print("connect succsesful")
+                    return("succses")  
+                else: 
+                    print("failed to connect")
+                    return("failed")
+                    s.close()
+            else:
+                print("failed to connect")
+                return("failed")
+                s.close()
+    USERID = "0"
+    def getcommandtyped():
+        global connected
+        commandinput = input().split()
+        commandtype = (commandinput[0])
+        if commandtype == ("connectcore"):
+            USERID = commandinput[1]
+            PASID = commandinput[2]
+            ADDRESS = commandinput[3]
+            PORT = commandinput[4]
+
+            if connected == False:
+                connect_core(USERID, PASID, ADDRESS, PORT)
+                connected = True
+                print("connected state:",connected)
+            else:
+                print("core is already connected would you like to recnnect to it? (yes/no)")
+                yesno = input()
+                if yesno == "yes":
+                    s.sendall(str.encode("disconnect"))
+                    s.close()
+                    if connect_core(USERID, PASID, ADDRESS, PORT) == "succses":
+                        print("reconnected")
+                        connected = True
+                    else:
+                        connected = False
+                        
+                if yesno == "no":
+                    print("wont make any changes")
+    while 1 == 1:
+        getcommandtyped()
+    def senddata(datatosend):
+        if connected == True:
+            str.encode(USERID), s.sendall(str.encode(datatosend))
