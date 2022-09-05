bae8a2d819248c1affa9073f2de8ad810c497445
Jeff Martin <jmartin127@gmail.com>
2022-09-05 15:37:54 -0600

    By default, start server

--- server.py
+++ server.py
@@ -4,18 +4,9 @@ conn = 0
 from dataclasses import dataclass
 import socket
 import uuid
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     def connect_base():
-        HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-        PORT = 5021  # Port to listen on (non-privileged ports are > 1023)
-        USERNAME = str(uuid.uuid4())
-        PASSWORD = str(uuid.uuid4())
-        print("username:", USERNAME,)
-        print("password:", PASSWORD)
-        print("connecting to:", PORT)
-        print("waiting for connectoin use the command","{connectcore",USERNAME,PASSWORD,HOST,PORT,"}to connect")
-        s.bind((HOST, PORT))
-        s.listen()
         conn, addr = s.accept()
         with conn:
             print(f"Connected by {addr}")
@@ -44,15 +35,30 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         commandtype = (commandinput[0])
         if commandtype == ("connectbase"):
             connect_base()
+        #else:
+        #    data = conn.recv(1024)
+        #    print("More data: {}".format(data))
             
-    while 1 == 1:
+
+    HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+    PORT = 5021  # Port to listen on (non-privileged ports are > 1023)
+    USERNAME = str(uuid.uuid4())
+    PASSWORD = str(uuid.uuid4())
+    print("username:", USERNAME,)
+    print("password:", PASSWORD)
+    print("connecting to:", PORT)
+    print("waiting for connection use the command","{connectcore",USERNAME,PASSWORD,HOST,PORT,"}to connect")
+    s.bind((HOST, PORT))
+    s.listen()
+
+    while True:
         getcommandtyped()
 
-    while 1 == 1:
-        data = conn.recv(1024)
-        print("Recieved data {}".format(data))
-        if data.decode() == "disconnect":
-            s.close()
+    # while 1 == 1:
+    #     data = conn.recv(1024)
+    #     print("Recieved data {}".format(data))
+    #     if data.decode() == "disconnect":
+    #         s.close()
 
             
 
